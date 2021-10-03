5fea109a268c66ce779ff418ee5e44a2cf8e1649
William Clifford <wobh@yahoo.com>
2021-10-03 13:42:58 -0400

    Trial setup of "succession" concept WIP
    - describe successions and collections in TODO
    - setup `make-chain-from` methods
      - create chain objects from sequences and hash-tables
    - try to clarify "successions". Native objects include:
      - lists :: as stacks
        - `peekf' <- `car'/`first'
        - `pullf'/`nextf' ,- `pop'
        - `pushf' <- `push'
      - character-streams :: as queues
        - `peekf' <- `peek-char'
        - `pullf'/`nextf' <- `read-char-no-hang'
        - `pushf' <- `write-char'

--- chains-test.lisp
+++ chains-test.lisp
@@ -10,148 +10,142 @@ passed."))
 
 (in-package #:org.wobh.common-lisp.tools.chains-test)
 
+(flet ((make-test (name subject expected actually &key (test #'eql))
+	 (assert (funcall test expected actually)
+		 (subject expected actually)
+		 "~A ~S~&~
+                  expected: ~A~&~
+                  actually: ~A"
+		 name
+		 subject
+		 expected
+		 actually)))
+
+
 ;;; Stack tests
 
-(let* ((subject (let ((a-stack (make-instance 'chains:stack)))
-		  (chains:pushf a-stack :foo)
-		  (chains:pushf a-stack :bar)
-		  (chains:pushf a-stack :baz)
-		  (chains:pushf a-stack :qux)))
-       (expected :qux)
-       (actually (chains:peekf subject)))
-  (assert (eql expected actually)
-	  (subject expected actually)
-	  "peekf stack ~A~&~
-          expected: ~A~&~
-          actually: ~A"
-	  subject
-	  expected
-	  actually))
+  (let ((subject (let ((a-stack (make-instance 'chains:stack)))
+		   (chains:pushf a-stack :foo)
+		   (chains:pushf a-stack :bar)
+		   (chains:pushf a-stack :baz)
+		   (chains:pushf a-stack :qux))))
+    (make-test 'sizef subject 4 (chains:sizef subject))
+    (make-test 'peekf subject :qux (chains:peekf subject))
+    (make-test 'nextf
+	       subject
+	       '(:qux :baz :bar :foo)
+	       (loop
+		 for item = (chains:nextf subject)
+		 while item
+		 collect item)
+	       :test #'equal))
 
-(let* ((subject (let ((a-stack (make-instance 'chains:stack)))
-		  (chains:pushf a-stack :foo)
-		  (chains:pushf a-stack :bar)
-		  (chains:pushf a-stack :baz)
-		  (chains:pushf a-stack :qux)))
-       (expected 4)
-       (actually (chains:sizef subject)))
-  (assert (eql expected actually)
-	  (subject expected actually)
-	  "sizef stack ~A~&~
-          expected: ~A~&~
-          actually: ~A"
-	  subject
-	  expected
-	  actually))
+  (let ((subject (chains:make-stack-from #(:foo :bar :baz))))
+    (check-type subject chains:stack)
+    (make-test 'sizef subject 3 (chains:sizef subject))
+    (make-test 'peekf subject
+		'(0 :foo)
+		(chains:peekf subject)
+		:test #'equal)
+    (make-test 'nextf
+	       subject
+	       '((0 :foo) (1 :bar) (2 :baz))
+	       (loop
+		 for item = (chains:nextf subject)
+		 while item
+		 collect item)
+	       :test #'equal))
 
-(let* ((subject (let ((a-stack (make-instance 'chains:stack)))
-		  (chains:pushf a-stack :foo)
-		  (chains:pushf a-stack :bar)
-		  (chains:pushf a-stack :baz)
-		  (chains:pushf a-stack :qux)))
-       (expected '(:qux :baz :bar :foo))
-       (actually (loop repeat 4 collect (chains:nextf subject))))
-  (assert (equal expected actually)
-	  (subject expected actually)
-	  "nextf stack ~A~&~
-          expected: ~A~&~
-          actually: ~A"
-	  subject
-	  expected
-	  actually))
+  (let ((subject (chains:make-chain-from #(:foo :bar :baz) 'chains:stack)))
+    (check-type subject chains:stack))
+
+  (let ((subject (chains:make-stack-from
+		  (let ((a-hash-table (make-hash-table)))
+		    (setf (gethash :bar a-hash-table) :foo)
+		    (setf (gethash :baz a-hash-table) :qux)
+		    a-hash-table))))
+    (check-type subject chains:stack)
+    (make-test 'sizef subject 2 (chains:sizef subject))
+    (make-test 'nextf
+	       subject
+	       '((:bar :foo) (:baz :qux))
+	       (loop
+		 for item = (chains:nextf subject)
+		 while item
+		 collect item into pairs
+		 finally
+		    (return (sort pairs #'string< :key #'first)))
+	       :test #'equal))
 
 
 ;;; Queue tests
 
-(let* ((subject (let ((a-queue (make-instance 'chains:queue)))
-		  (chains:pushf a-queue :foo)
-		  (chains:pushf a-queue :bar)
-		  (chains:pushf a-queue :baz)
-		  (chains:pushf a-queue :qux)))
-       (expected 4)
-       (actually (chains:sizef subject)))
-  (assert (eql expected actually)
-	  (subject expected actually)
-	  "sizef queue ~A~&~
-          expected: ~A~&~
-          actually: ~A"
-	  subject
-	  expected
-	  actually))
+  (let ((subject (let ((a-queue (make-instance 'chains:queue)))
+		   (chains:pushf a-queue :foo)
+		   (chains:pushf a-queue :bar)
+		   (chains:pushf a-queue :baz)
+		   (chains:pushf a-queue :qux))))
+    (make-test 'sizef subject 4 (chains:sizef subject))
+    (make-test 'peekf subject :foo (chains:peekf subject))
+    (make-test 'nextf
+	       subject
+	       '(:foo :bar :baz :qux)
+	       (loop
+		 for item = (chains:nextf subject)
+		 while item
+		 collect item)
+	       :test #'equal))
 
-(let* ((subject (let ((a-queue (make-instance 'chains:queue)))
-		  (chains:pushf a-queue :foo)
-		  (chains:pushf a-queue :bar)
-		  (chains:pushf a-queue :baz)
-		  (chains:pushf a-queue :qux)))
-       (expected :foo)
-       (actually (chains:peekf subject)))
-  (assert (eql expected actually)
-	  (subject expected actually)
-	  "peekf queue ~A~&~
-          expected: ~A~&~
-          actually: ~A"
-	  subject
-	  expected
-	  actually))
+  (let ((subject (chains:make-queue-from #(:foo :bar :baz))))
+    (check-type subject chains:queue)
+    (make-test 'sizef subject 3 (chains:sizef subject))
+    (make-test 'peekf subject
+		'(0 :foo)
+		(chains:peekf subject)
+		:test #'equal)
+    (make-test 'nextf
+	       subject
+	       '((0 :foo) (1 :bar) (2 :baz))
+	       (loop
+		 for item = (chains:nextf subject)
+		 while item
+		 collect item)
+	       :test #'equal))
 
-(let* ((subject (let ((a-queue (make-instance 'chains:queue)))
-		  (chains:pushf a-queue :foo)
-		  (chains:pushf a-queue :bar)
-		  (chains:pushf a-queue :baz)
-		  (chains:pushf a-queue :qux)))
-       (expected '(:foo :bar :baz :qux))
-       (actually (loop repeat 4 collect (chains:nextf subject))))
-  (assert (equal expected actually)
-	  (subject expected actually)
-	  "nextf queue ~A~&~
-          expected: ~A~&~
-          actually: ~A"
-	  subject
-	  expected
-	  actually))
+  (let ((subject (chains:make-chain-from #(:foo :bar :baz) 'chains:queue)))
+    (check-type subject chains:queue))
 
 
 ;;; Cycle tests
 
-(let* ((subject (let ((a-cycle (make-instance 'chains:cycle)))
-		  (chains:pushf a-cycle :foo)
-		  (chains:pushf a-cycle :bar)))
-       (expected :foo)
-       (actually (chains:peekf subject)))
-  (assert (eql expected actually)
-	  (subject expected actually)
-	  "peekf cycle ~A~&~
-          expected: ~A~&~
-          actually: ~A"
-	  subject
-	  expected
-	  actually))
+  (let ((subject (let ((a-cycle (make-instance 'chains:cycle)))
+		   (chains:pushf a-cycle :foo)
+		   (chains:pushf a-cycle :bar))))
+    (make-test 'sizef subject 2 (chains:sizef subject))
+    (make-test 'peekf subject :foo (chains:peekf subject))
+    (make-test 'nextf
+	       subject
+	       '(:foo :bar :foo :bar)
+	       (loop
+		 repeat 4
+		 collect (chains:nextf subject))
+	       :test #'equal))
 
-(let* ((subject (let ((a-cycle (make-instance 'chains:cycle)))
-		  (chains:pushf a-cycle :foo)
-		  (chains:pushf a-cycle :bar)))
-       (expected 2)
-       (actually (chains:sizef subject)))
-  (assert (eql expected actually)
-	  (subject expected actually)
-	  "sizef cycle ~A~&~
-          expected: ~A~&~
-          actually: ~A"
-	  subject
-	  expected
-	  actually))
+  (let ((subject (chains:make-cycle-from #(:foo :bar))))
+    (check-type subject chains:cycle)
+    (make-test 'sizef subject 2 (chains:sizef subject))
+    (make-test 'peekf
+	       subject
+	       '(0 :foo)
+	       (chains:peekf subject)
+	       :test #'equal)
+    (make-test 'nextf
+	       subject
+	       '((0 :foo) (1 :bar) (0 :foo) (1 :bar))
+	       (loop
+		 repeat 4
+		 collect (chains:nextf subject))
+	       :test #'equal))
 
-(let* ((subject (let ((a-cycle (make-instance 'chains:cycle)))
-		  (chains:pushf a-cycle :foo)
-		  (chains:pushf a-cycle :bar)))
-       (expected '(:foo :bar :foo :bar))
-       (actually (loop repeat 4 collect (chains:nextf subject))))
-  (assert (equal expected actually)
-	  (subject expected actually)
-	  "nextf cycle ~A~&~
-          expected: ~A~&~
-          actually: ~A"
-	  subject
-	  expected
-	  actually))
+  (let ((subject (chains:make-chain-from #(:foo :bar :baz) 'chains:cycle)))
+    (check-type subject chains:cycle)))

55cc4cfb30b2b977e93e3ee4a8f9378816cedd63
William Clifford <wobh@yahoo.com>
2021-10-03 13:42:58 -0400

    Trial setup of "succession" concept WIP
    - describe successions and collections in TODO
    - setup `make-chain-from` methods
      - create chain objects from sequences and hash-tables
    - try to clarify "successions". Native objects include:
      - lists :: as stacks
        - `peekf' <- `car'/`first'
        - `pullf'/`nextf' <- `pop'
        - `pushf' <- `push'
      - character-streams :: as queues
        - `peekf' <- `peek-char'
        - `pullf'/`nextf' <- `read-char-no-hang'
        - `pushf' <- `write-char'

--- chains-test.lisp
+++ chains-test.lisp
@@ -21,6 +21,137 @@ passed."))
 
 (in-package #:org.wobh.common-lisp.tools.chains-test)
 
+(flet ((make-test (name subject expected actually &key (test #'eql))
+	 (assert (funcall test expected actually)
+		 (subject expected actually)
+		 "~A ~S~&~
+                  expected: ~A~&~
+                  actually: ~A"
+		 name
+		 subject
+		 expected
+		 actually)))
+  (let ((subject (let ((a-stack (make-instance 'chains:stack)))
+		   (chains:pushf a-stack :foo)
+		   (chains:pushf a-stack :bar)
+		   (chains:pushf a-stack :baz)
+		   (chains:pushf a-stack :qux))))
+    (make-test 'sizef subject 4 (chains:sizef subject))
+    (make-test 'peekf subject :qux (chains:peekf subject))
+    (make-test 'nextf
+	       subject
+	       '(:qux :baz :bar :foo)
+	       (loop
+		 for item = (chains:nextf subject)
+		 while item
+		 collect item)
+	       :test #'equal))
+
+  (let ((subject (chains:make-stack-from #(:foo :bar :baz))))
+    (check-type subject chains:stack)
+    (make-test 'sizef subject 3 (chains:sizef subject))
+    (make-test 'peekf subject
+		'(0 :foo)
+		(chains:peekf subject)
+		:test #'equal)
+    (make-test 'nextf
+	       subject
+	       '((0 :foo) (1 :bar) (2 :baz))
+	       (loop
+		 for item = (chains:nextf subject)
+		 while item
+		 collect item)
+	       :test #'equal))
+
+  (let ((subject (chains:make-chain-from #(:foo :bar :baz) 'chains:stack)))
+    (check-type subject chains:stack))
+
+  (let ((subject (chains:make-stack-from
+		  (let ((a-hash-table (make-hash-table)))
+		    (setf (gethash :bar a-hash-table) :foo)
+		    (setf (gethash :baz a-hash-table) :qux)
+		    a-hash-table))))
+    (check-type subject chains:stack)
+    (make-test 'sizef subject 2 (chains:sizef subject))
+    (make-test 'nextf
+	       subject
+	       '((:bar :foo) (:baz :qux))
+	       (loop
+		 for item = (chains:nextf subject)
+		 while item
+		 collect item into pairs
+		 finally
+		    (return (sort pairs #'string< :key #'first)))
+	       :test #'equal))
+
+  (let ((subject (let ((a-queue (make-instance 'chains:queue)))
+		   (chains:pushf a-queue :foo)
+		   (chains:pushf a-queue :bar)
+		   (chains:pushf a-queue :baz)
+		   (chains:pushf a-queue :qux))))
+    (make-test 'sizef subject 4 (chains:sizef subject))
+    (make-test 'peekf subject :foo (chains:peekf subject))
+    (make-test 'nextf
+	       subject
+	       '(:foo :bar :baz :qux)
+	       (loop
+		 for item = (chains:nextf subject)
+		 while item
+		 collect item)
+	       :test #'equal))
+
+  (let ((subject (chains:make-queue-from #(:foo :bar :baz))))
+    (check-type subject chains:queue)
+    (make-test 'sizef subject 3 (chains:sizef subject))
+    (make-test 'peekf subject
+		'(0 :foo)
+		(chains:peekf subject)
+		:test #'equal)
+    (make-test 'nextf
+	       subject
+	       '((0 :foo) (1 :bar) (2 :baz))
+	       (loop
+		 for item = (chains:nextf subject)
+		 while item
+		 collect item)
+	       :test #'equal))
+
+  (let ((subject (chains:make-chain-from #(:foo :bar :baz) 'chains:queue)))
+    (check-type subject chains:queue))
+
+  (let ((subject (let ((a-cycle (make-instance 'chains:cycle)))
+		   (chains:pushf a-cycle :foo)
+		   (chains:pushf a-cycle :bar))))
+    (make-test 'sizef subject 2 (chains:sizef subject))
+    (make-test 'peekf subject :foo (chains:peekf subject))
+    (make-test 'nextf
+	       subject
+	       '(:foo :bar :foo :bar)
+	       (loop
+		 repeat 4
+		 collect (chains:nextf subject))
+	       :test #'equal))
+
+  (let ((subject (chains:make-cycle-from #(:foo :bar))))
+    (check-type subject chains:cycle)
+    (make-test 'sizef subject 2 (chains:sizef subject))
+    (make-test 'peekf
+	       subject
+	       '(0 :foo)
+	       (chains:peekf subject)
+	       :test #'equal)
+    (make-test 'nextf
+	       subject
+	       '((0 :foo) (1 :bar) (0 :foo) (1 :bar))
+	       (loop
+		 repeat 4
+		 collect (chains:nextf subject))
+	       :test #'equal))
+
+  (let ((subject (chains:make-chain-from #(:foo :bar :baz) 'chains:cycle)))
+    (check-type subject chains:cycle)))
+
+
 ;;; Stack tests
 
 (defun test-sizef-stack
@@ -64,6 +195,7 @@ passed."))
 	    subject
 	    expects
 	    results)))
+
 
 
 ;;; Queue tests
--- chains.lisp
+++ chains.lisp
@@ -6,6 +6,10 @@
   (:export #:chain #:stack #:queue #:cycle)
   (:export #:sizef #:peekf)
   (:export #:pushf #:pullf #:nextf)
+  (:export #:make-stack-from
+	   #:make-queue-from
+	   #:make-cycle-from
+	   #:make-chain-from)
   (:documentation "ORG.WOBH.COMMON-LISP.TOOLS.CHAINS
 
 Provides sequential structure types implemented with lists. I've tried
@@ -16,13 +20,26 @@ for the simplest formalization. Current classes:
 - queue :: a last-in-first-out (LIFO) structure
 - cycle :: a circular structure
 
-Some helpful methods which have also been defined for lists:
+Some factory methods have been defined for creating chain objects from
+other collection types:
 
-- sizef
-- peekf
-- pushf
-- pullf
-- nextf"))
+- make-chain-from : collection chain-type -> chain:chain-type
+- make-stack-from : collection -> stack
+- make-queue-from : collection -> queue
+- make-cycle-from : collection -> cycle
+
+These take a simple-vector or hash and turn it into one of the chain
+types whose items is a pair of values cooresponding to the index/key
+and value.
+
+Some other helpful methods for 'succession' objects which have also
+been defined for lists and streams:
+
+- peekf : succession -> item, succession
+- pullf : succession -> item, succession
+- nextf : succession -> item, succession
+- pushf : succession object -> succession
+"))
 
 (in-package #:org.wobh.common-lisp.tools.chains)
 
@@ -33,44 +50,59 @@ Some helpful methods which have also been defined for lists:
 
 Given a container, `sizef' returns the number of all elements in the
 container.")
-  (:method ((list list))
-    (list-length list)))
+  (:method ((container list))
+    (list-length container))
+  (:method ((container sequence))
+    (length container))
+  (:method ((container hash-table))
+    (hash-table-count container)))
 
-(defgeneric peekf (container)
+(defgeneric peekf (succession)
   (:documentation "peekf
 
-Given a container, `peekf' returns the 'first' object in the container,
-and the unmodified container.")
-  (:method ((list list))
-    (let ((out (car list)))
-      (values out list))))
+Given a succession, `peekf' returns the 'first' object in the succession,
+and the unmodified succession.")
+  (:method ((succession list))
+    (let ((out (car succession)))
+      (values out succession)))
+  (:method ((succession stream))
+    (let ((out (peek-char nil stream nil)))
+      (values out succession))))
 
-(defgeneric pullf (container)
+(defgeneric pullf (succession)
   (:documentation "pullf
 
-Given a container, `pullf' removes the 'first' object in the
-container, returning the removed object and modified container.")
-  (:method ((list list))
-    (let ((out (pop list)))
-      (values out list))))
+Given a succession, `pullf' removes the 'first' object in the
+succession, returning the removed object and modified succession.")
+  (:method ((succession list))
+    (let ((out (pop succession)))
+      (values out succession)))
+  (:method ((succession stream))
+    (let ((out (read-char-no-hang stream nil)))
+      (values out succession))))
 
-(defgeneric nextf (container)
+(defgeneric nextf (succession)
   (:documentation "nextf
 
-Given a container, `nextf' may remove the `first' object in the
-container, returning the object and container, which may be further
-modified according to the properties of the container. See: `cycle'.")
-  (:method ((list list))
-    (pullf list)))
+Given a succession, `nextf' may remove the `first' object in the
+succession, returning the object and succession, which may be further
+modified according to the properties of the succession. See: `cycle'.")
+  (:method ((succession list))
+    (pullf succession))
+  (:method ((succession stream))
+    (pullf succession)))
 
-(defgeneric pushf (container object)
+(defgeneric pushf (succession object)
   (:documentation "pushf
 
-Given a container and an object, `pushf' stores the object in the
-container and returns the container.")
-  (:method ((list list) object)
-    (push object list)
-    list))
+Given a succession and an object, `pushf' stores the object in the
+succession and returns the succession.")
+  (:method ((succession list) object)
+    (push object succession)
+    succession)
+  (:method ((succession stream) (object character))
+    (write-char object stream)
+    succession))
 
 
 ;; Chain
@@ -83,15 +115,23 @@ container and returns the container.")
           (make-load-form chain)
         (format stream "~W~@[~&~W~]" make-form setup-form))
       (if *print-escape*
-	  (print-unreadable-object (chain stream :type t :identity t)
-	    (format stream
+          (print-unreadable-object (chain stream :type t :identity t)
+            (format stream
                     (if *print-pretty*
                         "~<~@{~S ~S~^ ~@_~}~:>"
                         "~{~S ~S~^ ~}")
                     (cddr (make-load-form chain))))
-	  (format stream
+          (format stream
                   "~(~A~)"
                   (class-name (class-of chain))))))
+
+(defgeneric make-chain-from (collection chain-type)
+  (:method ((collection t) (chain-type (eql 'stack)))
+    (make-stack-from collection))
+  (:method ((collection t) (chain-type (eql 'queue)))
+    (make-queue-from collection))
+  (:method ((collection t) (chain-type (eql 'cycle)))
+    (make-cycle-from collection)))
 
 
 ;;; Stack
@@ -113,31 +153,49 @@ container and returns the container.")
   (declare (ignore environment))
   (with-slots (outgoing) stack
     `(make-instance ',(class-name (class-of stack))
-		    :items ,outgoing)))
+                    :items ,outgoing)))
 
-(defmethod sizef ((stack stack))
-  (with-slots (o-length) stack
+(defmethod sizef ((container stack))
+  (with-slots (o-length) container
     o-length))
 
-(defmethod peekf ((stack stack))
-  (with-slots (outgoing) stack
+(defmethod peekf ((succession stack))
+  (with-slots (outgoing) succession
     (let ((out (car outgoing)))
-      (values out stack))))
+      (values out succession))))
 
-(defmethod pullf ((stack stack))
-  (with-slots (outgoing o-length) stack
+(defmethod pullf ((succession stack))
+  (with-slots (outgoing o-length) succession
     (let ((out (pop outgoing)))
       (decf o-length)
-      (values out stack))))
+      (values out succession))))
 
-(defmethod nextf ((stack stack))
-  (pullf stack))
+(defmethod nextf ((succession stack))
+  (pullf succession))
 
-(defmethod pushf ((stack stack) object)
-  (with-slots (outgoing o-length) stack
+(defmethod pushf ((succession stack) object)
+  (with-slots (outgoing o-length) succession
     (push object outgoing)
     (incf o-length)
-    stack))
+    succession))
+
+(defgeneric make-stack-from (collection)
+  (:method ((collection simple-vector))
+    (loop
+      for index = 0 then (incf index)
+      for item across collection
+      collect (list index item) into a-list
+      finally
+         (return (make-instance 'stack
+                                :items a-list))))
+  (:method ((collection hash-table))
+    (loop
+      for index being the hash-key
+        using (hash-value item) of collection
+      collect (list index item) into a-list
+      finally
+         (return (make-instance 'stack
+                                :items a-list)))))
 
 
 ;;; Queue
@@ -151,47 +209,83 @@ container and returns the container.")
   (declare (ignore environment))
   (with-slots (outgoing incoming) queue
     `(make-instance ',(class-name (class-of queue))
-		    :items ,(append outgoing
-				    (reverse incoming)))))
+                    :items ,(append outgoing
+                                    (reverse incoming)))))
 
-(defmethod sizef ((queue queue))
-  (with-slots (o-length i-length) queue
+(defmethod sizef ((container queue))
+  (with-slots (o-length i-length) container
     (+ o-length i-length)))
 
-(defmethod peekf ((queue queue))
-  (with-slots (outgoing o-length incoming i-length) queue
+(defmethod peekf ((succession queue))
+  (with-slots (outgoing o-length incoming i-length) succession
     (let ((out (car outgoing)))
-      (values out queue))))
+      (values out succession))))
 
 (flet ((queue-turn (queue)
-	 (with-slots (outgoing o-length incoming i-length) queue
-	   (when (and (<= o-length i-length)
-		      (< 0 i-length))
-	     (setq outgoing
-		   (nconc outgoing
-			  (nreverse (shiftf incoming ()))))
-	     (incf o-length
-		   (shiftf i-length 0))))))
-  (defmethod pullf ((queue queue))
-    (with-slots (outgoing o-length) queue
+         (with-slots (outgoing o-length incoming i-length) queue
+           (when (and (<= o-length i-length)
+                      (< 0 i-length))
+             (setq outgoing
+                   (nconc outgoing
+                          (nreverse (shiftf incoming ()))))
+             (incf o-length
+                   (shiftf i-length 0))))))
+  (defmethod pullf ((succession queue))
+    (with-slots (outgoing o-length) succession
       (let ((out (pop outgoing)))
-	(decf o-length)
-	(queue-turn queue)
-	(values out queue))))
+        (decf o-length)
+        (queue-turn succession)
+        (values out succession))))
 
-  (defmethod pushf ((queue queue) object)
-    (with-slots (incoming i-length) queue
+  (defmethod pushf ((succession queue) object)
+    (with-slots (incoming i-length) succession
       (push object incoming)
       (incf i-length))
-    (queue-turn queue)
-    queue))
+    (queue-turn succession)
+    succession))
+
+(defgeneric make-queue-from (collection)
+  (:method ((collection simple-vector))
+    (loop
+      for index = 0 then (incf index)
+      for item across collection
+      collect (list index item) into a-list
+      finally
+         (return (make-instance 'queue
+                                :items a-list))))
+  (:method ((collection hash-table))
+    (loop
+      for index being the hash-key
+        using (hash-value item) of collection
+      collect (list index item) into a-list
+      finally
+         (return (make-instance 'queue
+                                :items a-list)))))
 
 
 ;;; Cycle
 
 (defclass cycle (queue) ())
 
-(defmethod nextf ((cycle cycle))
-  (let ((out (pullf cycle)))
-    (pushf cycle out)
-    (values out cycle)))
+(defmethod nextf ((succession cycle))
+  (let ((out (pullf succession)))
+    (pushf succession out)
+    (values out succession)))
+
+(defgeneric make-cycle-from (collection)
+  (:method ((collection simple-vector))
+    (loop
+      for index = 0 then (incf index)
+      for item across collection
+      collect (list index item) into a-list
+      finally
+         (return (make-instance 'cycle
+                                :items a-list))))
+  (:method ((collection hash-table))
+    (loop
+      for index being the hash-key
+        using (hash-value item) of collection
+      collect (list index item) into a-list
+      finally
+         (return (make-instance 'cycle
+                                :items a-list)))))
